34e82968f71eac66f1be97b9341b434fa2c9153a
Marcelo Junqueira Teixeira <marcelojxt@gmail.com>
2023-07-06 22:01:11 -0300

    [M1S08] Ex 1

---
+++ js/dados.js
@@ -0,0 +1,19 @@
+const usuarios = `
+  [
+    {
+      "nome": "Marcelo",
+      "email": "marcelo@teste.com",
+      "senha" : 1234
+    },
+    {
+      "nome": "Maria",
+      "email": "maria@teste.com",
+      "senha" : 4321
+    },
+    {
+      "nome": "João",
+      "email": "joao@teste.com",
+      "senha" : 5678
+    }
+  ]
+`
